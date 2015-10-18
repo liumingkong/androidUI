7735853984757507416abf37ac854172f3520cf7
liumingkong <liumingkong@sina.com>
2015-10-18 20:40:08 +0800

    update android ui

---
+++ app/src/main/java/com/mico/constants/ModelConstants.java
@@ -0,0 +1,262 @@
+package com.mico.constants;
+
+/**
+ * Created by liumingkong on 14-4-23.
+ */
+public class ModelConstants {
+
+    public static final String SINGLE_NULL = "{}";
+    public static final String MULTI_NULL = "[]";
+    public static final String NUM = "num";
+
+    //result
+    public static final String ACCOUNT = "account";
+    public static final String SECRETKEY = "secretKey";
+    public static final String ENDPOINT = "endpoint";
+    public static final String RESULT = "result";
+    public static final String FLAG = "flag";
+    public static final String CREATE_TIME = "createTime";
+    //time
+    public static final String LAST_UPDATE = "lastUpdate";
+    public static final String TIMESTAMP = "timestamp";
+
+    public static final String OWNER = "owner";
+    public static final String OWNER_ID = "ownerId";
+    public static final String POINT = "point";
+    public static final String RELATION = "relation";
+
+    public static final String SCHOOL = "school";
+    public static final String TELEPHONE = "telephone";
+    public static final String TIME = "time";
+    public static final String TYPE = "type";
+    public static final String CONTENT = "content";
+    public static final String CONTACTS = "contacts";
+
+    // 群相关私有信息
+    public static final String GROUPID = "groupId";
+    public static final String GROUP_NAME = "groupName";
+    public static final String GROUP_TYPE = "groupType";
+    public static final String GROUP_OWNERID = "ownerId";
+    public static final String GROUP_LEVEL = "level";
+    public static final String GROUP_MEMBERS = "members";
+    public static final String GROUP_LOCATION = "location";
+    public static final String GROUP_PRIVACY = "privacy";
+    public static final String COMPANY = "company";
+    public static final String CONSTELLATION = "constellation";
+    public static final String COORDINATES = "coordinates";
+    public static final String DISPLAY_NAME = "displayName";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String NO = "n";
+    public static final String YES = "y";
+
+    // 个人相关的信息
+    public static final String UID = "uid";
+    public static final String DISPLAYNAME = "displayName";
+    public static final String GENDAR = "gendar";
+    public static final String AGE = "age";
+    public static final String BIRTHDAY = "birthday";
+    public static final String LOCTIME = "loctime";
+    public static final String LEVEL = "level";
+    public static final String END = "end";
+    public static final String VISIT_TIME = "visit_timestamp";
+    public static final String STATUS = "status";
+    public static final String USERID = "userId";
+    public static final String CAREER = "career";
+    public static final String HOBBY = "hobby";
+
+    public static final String NEW_PWD = "newPassword";
+    public static final String OLD_PWD = "oldPassword";
+
+    public static final String TARGET_UID = "targetUid";
+
+    // 共用
+    public static final String EXTEND = "extend";
+
+    public static final String AVATAR = "avatar";
+    public static final String DESCRIPTION = "description";
+    public static final String INTEREST = "interest";
+    public static final String LOCATE_DESC = "locate_desc";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String LASTUPDATE = "lastUpdate";
+    public static final String REGION = "region";
+    public static final String AUDIO_INTRO = "audio_intro";
+    public static final String PHOTO_WALL_BG = "pwb";
+    public static final String DEFAULT_PHOTO_WALL_BG = "vip_photo_wall_bg.png";
+
+    public static final String IS_MEMBER = "isMember";
+    public static final String DISTANCE = "distance";
+
+    public static final String CONV_ID = "convId";
+    public static final String MSG_ID = "msgId";
+
+    public static final String PRIVACY = "privacy";
+    //地图用
+    public static final String READONLY = "readonly";
+    public static final String REASON = "reason";
+
+    public static final String CHATID = "chatId";
+
+    public static final String VERSION = "version";
+    public static final String URL = "url";
+
+    // feed
+    public static final String FEEDID = "cid";
+    public static final String FEED_TEXT = "text";
+    public static final String FEED_FIDS = "fids";
+    public static final String FEED_LIKE_COUNT = "likeCount";
+    public static final String FEED_COMMENT_COUNT = "commentCount";
+    public static final String FEED_LIKE_EXIST = "isLiked";
+    public static final String FEED_TYPE = "type";
+    public static final String FEED_PRIVACY = "privacy";
+    public static final String FEED_OWNERID = "ownerId";
+    public static final String LIKE_TIME = "like_timestamp";
+    public static final String COUNT = "count";
+    public static final String IS_LIKED = "isLiked";
+    public static final String LIKES = "likes";
+    public static final String FEED_CARD_ICON = "icon";
+    public static final String FEED_CARD_TITLE = "title";
+    public static final String FEED_CARD_CONTENT = "content";
+
+    public static final String FEED_INFO = "FEED_INFO";
+    public static final String COMMENT_INFO = "COMMENT_INFO";
+
+    public static final String FEED_STATE_CHANGE = "FEED_STATE_CHANGE";
+
+    // comment
+    public static final String COMMENTS = "comments";
+    public static final String COMMENTID_GET = "comment_id";
+    public static final String COMMENTID_POST = "commentId";
+    public static final String COMMENTTIME_POST = "timestamp";
+    public static final String COMMENTTEXT_POST = "content";
+    public static final String FROMUSER = "from_user";
+    public static final String TOUSER = "to_user";
+    public static final String FROMUID = "fromUid";
+    public static final String TOUID = "toUid";
+    public static final String COMMENTTEXT_GET = "comment_content";
+    public static final String COMMENTTIME_GET = "comment_timestamp";
+    public static final String COMMENT_TARGETID = "targetUid"; //被评论的人，如果是直接评论一个动态，那么targetUid就是动态的作者，如果是作者回复一条评论，那么targetUid就是被回复的那个人
+
+    // apkinfo
+    public static final String MD5 = "md5";
+    public static final String UPDATEDATE = "updatedate";
+    public static final String SIZE = "size";
+
+    //region 复合数据结构section常量
+    public static final String GROUP = "group";
+    public static final String LOCATION = "location";
+    public static final String USER = "user";
+    public static final String CIRCLE = "circle";
+    //endregion
+    public static final String FEEDINFOVO_ADD = "feedInfoVO_add";
+
+    public static final String IP = "ip";
+    public static final String PORT = "port";
+
+    //计数相关
+
+    public static final String FIDS ="fids";
+    public static final String OLD_IMAGE_PATH ="old_image_path";
+    public static final String FID_LOCAL ="fid_local";
+    public static final String IS_UPLOAD = "isUpload";
+    public static final String FANS_COUNT ="fans_count";
+
+    public static final String PERMISSION = "permission";
+    public static final String NAME = "name";
+    public static final String VALUE = "value";
+    public static final String PHOTO_WALL_NUM = "PHOTO_WALL_NUM";
+//    public static final String AUDIO_INTRO = "AUDIO_INTRO";
+    public static final String CREATE_GROUP = "CREATE_GROUP";
+
+    public static final String PHOTO_WALL = "photowall";
+    public static final String LATEST_CIRCLE = "latest_circle";
+    public static final String PHOHO_WALL_COUNT = "photowall_count";
+    public static final String CIRCLE_COUNT = "circle_count";
+
+    public static final String FAV_COUNT = "favCount";
+    public static final String FAVED_COUNT = "favedCount";
+    public static final String FRIEND_COUNT = "friendCount";
+
+    public static final String FACEBOOK = "facebook";
+    public static final String GOOGLEPLUS = "google";
+    public static final String SOCIAL_ACCOUNT = "social_account";
+
+
+    public static final String IMAGE = "image";
+    public static final String ACTION = "action";
+    public static final String BODY = "body";
+    public static final String TICKER = "ticker";
+    public static final String TITLE = "title";
+
+    public static final String ID = "id";
+    public static final String FID = "fid";
+    public static final String COVER_FID = "coverFid";
+    public static final String LINK = "link";
+    public static final String IS_FREE = "isFree";
+    public static final String IS_NEW = "isNew";
+    public static final String IS_VIP = "isVip";
+    public static final String TAB_COVER = "tabCover";
+
+    public static final String PACK_INFO = "packInfo";
+    public static final String PASTER_INFO = "pasterInfo";
+
+
+    public static final String MICO = "mico";
+    public static final String PASTER_FID = "pasterFid";
+    public static final String PASTER_PACK_ID = "pasterPackId";
+    public static final String PASTER_ID = "pasterId";
+    public static final String PASTER_TYPE = "pasterType";
+    public static final String PASTER_COVER_FID = "pasterCoverFid";
+    public static final String PACKAGE_NAME = "packageName";
+    public static final String TARGET_NAME = "targetName";
+    public static final String IS_VISIBLE = "isVisible";
+    public static final String IS_FIRST = "isFirst";
+    public static final String IS_HOT = "isHot";
+    public static final String IS_COOLSYMBOL = "isCoolSymbol";
+    public static final String IS_LIKE = "isLike";
+    public static final String IS_BUTTON_CLICK = "IS_BUTTON_CLICK";
+
+    public static final String FROM_TAG = "FROM_TAG";
+    public static final String FOLDERID = "folderId";
+
+    public static final String COUNTRY = "country";
+    public static final String CITY = "city";
+    public static final String LAT = "lat";
+    public static final String LNG = "lng";
+    public static final String USERROAM_PLANE = "userroam_plane";
+    public static final String RECEIPT = "receipt";
+    public static final String APN = "apn";
+    public static final String NET_TYPE = "netType";
+
+    public static final String GOODS = "goods";
+    public static final String TITLE_INTRO = "titleIntro";
+    public static final String DESC = "desc";
+    public static final String PRICE = "price";
+    public static final String GOOGLE_ID = "googleId";
+    public static final String PURCHASE_TYPE = "purchaseType";
+
+    public static final String NONCE = "nonce";
+    public static final String ENCRYPTION = "encryption";
+    public static final String TOKEN = "token";
+
+
+    public static final String FROM_PAGE = "fromPage";
+
+    public static final String VERSION_AR 	= "versionAr";
+    public static final String MD5_AR		= "md5Ar";
+    public static final String SIZE_AR 		= "sizeAr";
+    public static final String URL_AR 		= "urlAr";
+    public static final String LOCALE 		= "locale";
+    public static final String IS_VIP_GIFT 		= "isVipGift";
+
+    /** roam ****/
+    public static final String RESULTS = "results";
+    public static final String FORMATTED_ADDRESS = "formatted_address";
+    public static final String TYPES = "types";
+    public static final String STREET_ADDRESS = "street_address";
+    public static final String IS_EFFECTIVE = "IS_EFFECTIVE";
+
+
+    public static final String FROM_CAPTURE ="fromCapture";
+}
---
+++ app/src/main/java/com/mico/model/db/Generator.java
@@ -0,0 +1,114 @@
+package com.mico.model.db;
+
+import com.mico.constants.ModelConstants;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+/**
+ * Created by zhoumingjun on 3/17/14.
+ */
+public class Generator {
+    public static final String SCHEMA_NAME = "com.mico.model.po";
+    public static final int SCHEMA_VERSION = 4;
+
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(SCHEMA_VERSION, SCHEMA_NAME);
+        addUserProfile(schema);
+        addConversation(schema);
+        addChatMessage(schema);
+        addGroupProfile(schema);
+        addSetting(schema);
+        addRelation(schema);
+        addChatRead(schema);
+        addTranslate(schema);
+        new DaoGenerator().generateAll(schema, "app/src/main/java");
+    }
+
+    private static void addUserProfile(Schema schema) {
+        Entity userProfile = schema.addEntity("UserProfilePO");
+        userProfile.addLongProperty(ModelConstants.UID).primaryKey().index();
+        userProfile.addIntProperty(ModelConstants.GENDAR).notNull();
+        userProfile.addStringProperty(ModelConstants.DISPLAY_NAME).notNull();
+        userProfile.addStringProperty(ModelConstants.AVATAR);
+        userProfile.addStringProperty(ModelConstants.DESCRIPTION);
+        userProfile.addLongProperty(ModelConstants.CREATE_TIME);
+        userProfile.addLongProperty(ModelConstants.BIRTHDAY);
+        userProfile.addIntProperty(ModelConstants.LEVEL);
+        userProfile.addStringProperty(ModelConstants.EXTEND);
+    }
+
+    private static void addConversation(Schema schema) {
+        Entity conv = schema.addEntity("ConversationPO");
+        conv.addLongProperty("convId").primaryKey().index();
+        conv.addIntProperty("type").index().notNull();
+        conv.addStringProperty("lastMessageId"); //最后一次更新的时间
+        conv.addLongProperty("lastUpdateTime").index().notNull(); // 单人聊天为信息摘要，群聊为 发送者:信息内容
+        conv.addStringProperty("lastUpdateMessage");
+        conv.addIntProperty("unreadCount"); // 当前群的未读数
+        conv.addIntProperty("lastUpdateStatus"); // last update status
+        conv.addStringProperty("convSetting"); // 会话设置
+        conv.addStringProperty("ext");
+    }
+
+    private static void addChatMessage(Schema schema) {
+        Entity ChatMessage = schema.addEntity("ChatMessagePO");
+        ChatMessage.addStringProperty("msgId").primaryKey().index();
+        ChatMessage.addLongProperty("convId").index().notNull();
+        ChatMessage.addLongProperty("uid").index().notNull();
+        ChatMessage.addIntProperty("direction").notNull();
+        ChatMessage.addIntProperty("type");
+        ChatMessage.addIntProperty("status");
+        ChatMessage.addLongProperty("ctime").index().notNull();
+        ChatMessage.addStringProperty("ext");
+    }
+
+    private static void addGroupProfile(Schema schema) {
+        Entity relation = schema.addEntity("GroupProfilePO");
+        relation.addLongProperty(ModelConstants.GROUPID).primaryKey().index(); // 群ID
+        relation.addIntProperty(ModelConstants.GROUP_TYPE); // 群类型
+        relation.addLongProperty(ModelConstants.GROUP_OWNERID);   // 群主
+        relation.addStringProperty(ModelConstants.GROUP_NAME); // 群名称
+        relation.addStringProperty(ModelConstants.AVATAR);    // 群头像
+        relation.addStringProperty(ModelConstants.DESCRIPTION); // 群简介
+        relation.addIntProperty(ModelConstants.GROUP_LEVEL);    // 群等级
+        relation.addStringProperty(ModelConstants.GROUP_MEMBERS); // 群成员
+        relation.addStringProperty(ModelConstants.GROUP_LOCATION); // 群地址描述
+        relation.addIntProperty(ModelConstants.GROUP_PRIVACY); // 群隐私
+        relation.addStringProperty(ModelConstants.EXTEND);    // 扩展信息
+        relation.addLongProperty(ModelConstants.CREATE_TIME);  // 群创建信息
+    }
+
+    private static void addSetting(Schema schema) {
+        Entity relation = schema.addEntity("SettingPO");
+        relation.addStringProperty("key").primaryKey().notNull().index();
+        relation.addStringProperty("value");
+    }
+
+    private static void addRelation(Schema schema) {
+        Entity userProfile = schema.addEntity("RelationPO");
+        userProfile.addLongProperty(ModelConstants.UID).primaryKey().index();
+        userProfile.addIntProperty(ModelConstants.TYPE).notNull().index();
+        userProfile.addLongProperty(ModelConstants.LASTUPDATE).notNull().index();
+    }
+
+    private static void addChatRead(Schema schema) {
+        Entity chatRead = schema.addEntity("ChatReadPO");
+        chatRead.addStringProperty("msgId").primaryKey().index();
+        chatRead.addLongProperty("convId").index().notNull();
+        chatRead.addLongProperty("ctime").index().notNull();
+        chatRead.addStringProperty("ext");
+    }
+
+    private static void addTranslate(Schema schema) {
+        Entity translate = schema.addEntity("TranslatePO");
+        translate.addIdProperty().primaryKey();
+        translate.addStringProperty("translateIndex").index().notNull();
+        translate.addStringProperty("originText").index().notNull();
+        translate.addStringProperty("translateText").index().notNull();
+        translate.addStringProperty("locale").notNull();
+        translate.addIntProperty("type");
+        translate.addStringProperty("ext");
+    }
+}
